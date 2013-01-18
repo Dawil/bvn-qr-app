f3cf4ff6131bf0e23b2e247c9850ee544f4f5b33
dave <dave@davidgwilcox.com>
2013-01-18 14:22:50 +1100

    the auth service now creates the correct url for DavesQrApp

--- app/scripts/services/auth.ts
+++ app/scripts/services/auth.ts
@@ -7,12 +7,22 @@ interface IAuthService {
   isLoggedIn: () => bool;
 }
 
-bvnQrApp.factory('auth', function():IAuthService {
+bvnQrApp.factory('auth', <any[]>['$window',
+		function($window):IAuthService {
+	var params = [ ["response_type"		, "token"]
+							 , ["client_id"				, "640956945398.apps.googleusercontent.com"]
+							 , ["redirect_uri"		, $window.escape("http://localhost:3501/oauth2callback")]
+							 , ["scope"						, $window.escape("https://spreadsheets.google.com/feeds/")]
+							 , ["approval_prompt"	, "auto"]
+							 ],
+			paramsString = params.map( (keyValue) => keyValue.join("=") ).join("&"),
+			baseUrl = "https://accounts.google.com/o/oauth2/auth",
+			googleApiUrl = [baseUrl, paramsString].join("?");
 	return {
-		logInUrl: "https://google.com", //TODO FIXME
+		logInUrl: googleApiUrl,
 		logOut: () => {
 		},
 		isLoggedIn: () => {
 		}
 	};
-});
+}]);
