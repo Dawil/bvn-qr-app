e859c052181a2500283165e490d7578d37d45760
dave <dave@davidgwilcox.com>
2013-01-23 12:29:02 +1100

    changed spreadsheet service structure
    It is now easier to use the spreadsheet service as a source of truth.
    That is, rows and other data in the spreadsheet service are now only
    accessible via getters.
    
    This is so it'll be a more natural fit when I start adding setters.

--- app/scripts/controllers/sessions.ts
+++ app/scripts/controllers/sessions.ts
@@ -10,18 +10,11 @@ interface ISessionsScope {
 bvnQrApp.controller('SessionsCtrl',
 		<any[]>['$scope', 'auth', 'spreadsheets',
 		function($scope, auth, spreadsheets) {
-			spreadsheets.getSpreadsheetData(auth.accessToken())
-				.then((result) => {
-					$scope.sessionSheet = result.attendanceSheet;
-					$scope.formSheet = result.formSheet;
-					console.log(result);
-					spreadsheets.getRows($scope.sessionSheet,
-						auth.accessToken())
-						.then((result) => {
-								console.log(result);
-								$scope.sessionRows = result.data.feed.entry;
-							}, (error) => { console.log(error) });
-				}, (error) =>  { console.log(error) });
+			spreadsheets.syncData(auth.accessToken());
+
+			$scope.getRows = spreadsheets.getRows;
+			$scope.getSpreadsheet = spreadsheets.getSpreadsheet;
+			$scope.getFormSheet = spreadsheets.getFormSheet;
 		}
 	]
 );
--- app/scripts/services/spreadsheets.ts
+++ app/scripts/services/spreadsheets.ts
@@ -2,60 +2,86 @@
 'use strict';
 
 interface ISpreadsheetsService {
-	getSpreadSheetData: (string) => ng.IPromise;
-	getRows: (sheetId) => ng.IPromise;
+	syncData: (string) => ng.IPromise;
+	getRows: any;
+	getSpreadsheet: any;
+	getFormSheet: any;
 }
 
 bvnQrApp.factory('spreadsheets',
 		<any[]>['$http', '$q',
 		function($http, $q) {
-			return {
-				getSpreadsheetData: (accessToken) => {
-					var API_URL:string = "https://www.googleapis.com/drive/v2",
-							attendanceSheet,
-							formSheet;
-					var deferred = $q.defer();
-					var success = (result) => {
-						// see http://typescript.codeplex.com/workitem/631
-						// aka triple redundancy!!
-						var console;
-						var console;
-						var sheets = result.data.items;
+			var _attendanceSheet = {
+				metaData: null,
+				rows: null
+			}, _formSheet,
+			loadSpreadsheetData = (accessToken) => {
+				var console, console;
+				var API_URL:string = "https://www.googleapis.com/drive/v2";
+				var deferred = $q.defer();
+				var success = (result) => {
+					// see http://typescript.codeplex.com/workitem/631
+					// aka triple redundancy!!
+					var console;
+					var console;
+					var sheets = result.data.items;
+					var formSheet, attendanceSheetMetaData;
 
-						for (var i = 0; i < sheets.length; i++) {
-							if (sheets[i].title.search("QR App Test") == 0) {
-								if (sheets[i].title.search("Form") != -1) {
-									formSheet = sheets[i];
-								} else if (sheets[i].title.search("Attendance") != -1) {
-									attendanceSheet = sheets[i];
-								}
+					for (var i = 0; i < sheets.length; i++) {
+						if (sheets[i].title.search("QR App Test") == 0) {
+							if (sheets[i].title.search("Form") != -1) {
+								formSheet = sheets[i];
+							} else if (sheets[i].title.search("Attendance") != -1) {
+								attendanceSheetMetaData = sheets[i];
 							}
 						}
-						deferred.resolve({
-							attendanceSheet: attendanceSheet,
-							formSheet: formSheet
+					}
+					deferred.resolve({
+						attendanceSheetMetaData: attendanceSheetMetaData,
+						formSheet: formSheet
+					});
+				}, fail = (error) => {
+					console.log(error);
+				};
+				$http.jsonp(API_URL + "/files?access_token=" + accessToken + "&callback=JSON_CALLBACK")
+					.then(success, fail);
+				return deferred.promise;
+			}, loadRows = (sheet, accessToken) => {
+				console.log(sheet);
+				var API_URL = "https://spreadsheets.google.com/feeds/list/"
+												+ sheet.id + "/od6/private/values?alt=json&access_token=" + accessToken + "&callback=JSON_CALLBACK",
+						deferred = $q.defer();
+				$http.jsonp(API_URL)
+					.then((result) => {
+							deferred.resolve(result);
+						}, (error) => {
+							console.log(error);
+							deferred.reject(error);
 						});
-					}, fail = (error) => {
-						console.log(error);
-					};
-					$http.jsonp(API_URL + "/files?access_token=" + accessToken + "&callback=JSON_CALLBACK")
-						.then(success, fail);
-					return deferred.promise;
-				},
-				getRows: (sheet, accessToken) => {
-					var API_URL = "https://spreadsheets.google.com/feeds/list/"
-													+ sheet.id + "/od6/private/values?alt=json&access_token=" + accessToken + "&callback=JSON_CALLBACK",
-/*							"https://www.googleapis.com/drive/v2/files/"
-								+ sheet.id + "?access_token=" + accessToken,*/
-							deferred = $q.defer();
-					$http.jsonp(API_URL)
+				return deferred.promise;
+			};
+
+			return {
+				syncData: (accessToken) => {
+					var deferred = $q.defer();
+					loadSpreadsheetData(accessToken)
 						.then((result) => {
-								deferred.resolve(result);
-							}, (error) => {
-								console.log(error);
-								deferred.reject(error);
-							});
+							console.log(result);
+							_attendanceSheet.metaData = result.attendanceSheetMetaData;
+							_formSheet = result.formSheet;
+							loadRows(_attendanceSheet.metaData, accessToken)
+								.then((result) => {
+									_attendanceSheet.rows = result;
+									deferred.resolve();
+								}, (error) => { console.log(error) });
+						}, (error) => { console.log( error ) } );
 					return deferred.promise;
+				}, getRows: () => {
+					return _attendanceSheet.rows;
+				}, getSpreadsheet: () => {
+					return _attendanceSheet.metaData;
+				}, getFormSheet: () => {
+					return _formSheet;
 				}
 			};
 		}
